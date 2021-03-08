5e35ca543f7488970dc18d418dd2ec824ae35f89
mahmoudzaher00 <hoda.zaher999@gmail.com>
2021-03-09 01:05:29 +0200

    a7a

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_an3ami/screens/HomeScreen.dart';
+import 'package:flutter_app_an3ami/screens/n3imy.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter marwan',
       theme: ThemeData(),
-      home: HomeScreen(),
+      home: page_one(),
     );
   }
 }
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    Scaffold(
+   return Scaffold(
      appBar: AppBar(),
       body: Container(),
     );
