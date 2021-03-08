b3a554f01a138599e2378fbef864685243200744
mahmoudzaher00 <hoda.zaher999@gmail.com>
2021-03-08 23:31:51 +0200

    msa

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter zaher',
+      title: 'Flutter zaherr',
       theme: ThemeData(),
       home: HomeScreen(),
     );
