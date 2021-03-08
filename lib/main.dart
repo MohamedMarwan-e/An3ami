3bb219103aa194c3ac1b5f95af9be5608c90dfae
MohamedMarwan-e <mohamedmarwango@gmail.com>
2021-03-08 23:36:08 +0200

    testmo22

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter zaherr',
+      title: 'Flutter marwan',
       theme: ThemeData(),
       home: HomeScreen(),
     );
